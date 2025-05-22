b49e34a464a88db8433f3d83446ce968d47b3358
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-05-22 15:41:57 +0200

    wip

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "solo";
+
+  inputs = {
+    # TODO: drop `/module` after https://github.com/Gepetto/nix/pull/54
+    gepetto.url = "github:gepetto/nix/module";
+    flake-parts.follows = "gepetto/flake-parts";
+    nixpkgs.follows = "gepetto/nixpkgs";
+    nix-ros-overlay.follows = "gepetto/nix-ros-overlay";
+    systems.follows = "gepetto/systems";
+    treefmt-nix.follows = "gepetto/treefmt-nix";
+    # TODO: use gepetto/ ?
+    utils = {
+      url = "github:Gepetto/nix-lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [ inputs.gepetto.flakeModule ];
+      perSystem =
+        {
+          lib,
+          pkgs,
+          self',
+          ...
+        }:
+        {
+          packages = {
+            default = self'.packages.solo;
+            solo = pkgs.python3Packages.buildPythonPackage {
+              format = "pyproject";
+              pname = "robot_properties_solo";
+              version = inputs.utils.lib.pythonVersion pkgs ./pyproject.toml;
+
+              nativeBuildInputs = [ pkgs.python3Packages.setuptools pkgs.rosPackages.rolling.xacro ];
+
+              buildInputs = with pkgs.python3Packages; [ meshcat pybullet  ];
+
+              src = builtins.path {
+                name = "src";
+                path = ./.;
+              };
+            };
+          };
+        };
+    };
+}
